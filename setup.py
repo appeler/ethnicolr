c4cecfc25103cf0b5644c8f19ab1bcc0bc3548b7
Suriyan Laohaprapanon <suriyant@gmail.com>
2017-05-29 11:05:45 +0700

    add Python 2.7 support and bump to 0.1.2

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1.2',
 
     description='Predict Race/Ethnicity Based on Name',
     long_description=long_description,
@@ -90,6 +90,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries :: Python Modules',
