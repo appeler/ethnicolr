b4449074e09f7336738abf08ac75c8ea3f01d399
Suriyan Laohaprapanon <suriyant@gmail.com>
2021-06-22 17:11:49 +0000

    bump to version 0.6.0

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.0',
+    version='0.6.0',
 
     description='Predict Race/Ethnicity Based on Sequence of Characters in the Name',
     long_description=long_description,
@@ -90,6 +90,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries :: Python Modules',
