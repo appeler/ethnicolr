c9901628e5ee0da5e4ef315dcb07f9aadc78932d
BasharNaji <balkuwai@gmail.com>
2021-10-15 19:24:49 -0700

    Notebook for Florida Last Name uncertainty model

--- ethnicolr/__init__.py
+++ ethnicolr/__init__.py
@@ -4,6 +4,7 @@ from .pred_census_ln import pred_census_ln
 from .pred_wiki_ln import pred_wiki_ln
 from .pred_wiki_name import pred_wiki_name
 from .pred_fl_reg_ln import pred_fl_reg_ln
+from .pred_fl_reg_ln_cn_int import pred_fl_reg_ln_cn_int
 from .pred_fl_reg_name import pred_fl_reg_name
 from .pred_nc_reg_name import pred_nc_reg_name
 from .pred_fl_reg_ln_five_cat import pred_fl_reg_ln_five_cat
@@ -11,5 +12,5 @@ from .pred_fl_reg_name_five_cat import pred_fl_reg_name_five_cat
 
 
 __all__ = ['census_ln', 'pred_census_ln', 'pred_wiki_ln', 'pred_wiki_name',
-           'pred_fl_reg_ln', 'pred_fl_reg_name', 'pred_nc_reg_name',
+           'pred_fl_reg_ln', 'pred_fl_reg_ln_cn_int', 'pred_fl_reg_name', 'pred_nc_reg_name',
            'pred_fl_reg_ln_five_cat', 'pred_fl_reg_name_five_cat']
