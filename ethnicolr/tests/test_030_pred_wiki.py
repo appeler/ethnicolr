e89b1b0e139ee28bac91adc97627ed2a4b2eaec5
BasharNaji <balkuwai@gmail.com>
2021-10-27 18:58:09 -0700

    Fixed test scripts to use array of races to sum across

--- ethnicolr/tests/test_030_pred_wiki.py
+++ ethnicolr/tests/test_030_pred_wiki.py
@@ -14,27 +14,58 @@ from ethnicolr.pred_wiki_ln import pred_wiki_ln
 from ethnicolr.pred_wiki_name import pred_wiki_name
 from . import capture
 
+race = [
+    "Asian,GreaterEastAsian,EastAsian_mean",
+    "Asian,GreaterEastAsian,Japanese_mean",
+    "Asian,IndianSubContinent_mean",
+    "GreaterAfrican,Africans_mean",
+    "GreaterAfrican,Muslim_mean",
+    "GreaterEuropean,British_mean",
+    "GreaterEuropean,EastEuropean_mean",
+    "GreaterEuropean,Jewish_mean",
+    "GreaterEuropean,WestEuropean,French_mean",
+    "GreaterEuropean,WestEuropean,Germanic_mean",
+    "GreaterEuropean,WestEuropean,Hispanic_mean",
+    "GreaterEuropean,WestEuropean,Italian_mean",
+    "GreaterEuropean,WestEuropean,Nordic_mean",
+]
+
 
 class TestPredWiki(unittest.TestCase):
-
     def setUp(self):
-        names = [{'last': 'smith', 'first': 'john', 'true_race': 'GreaterEuropean,British'},
-                 {'last': 'zhang', 'first': 'simon', 'true_race': 'Asian,GreaterEastAsian,EastAsian'}]
+        names = [
+            {"last": "smith", "first": "john", "true_race": "GreaterEuropean,British"},
+            {
+                "last": "zhang",
+                "first": "simon",
+                "true_race": "Asian,GreaterEastAsian,EastAsian",
+            },
+        ]
         self.df = pd.DataFrame(names)
 
     def tearDown(self):
         pass
 
     def test_pred_wiki_ln(self):
-        odf = pred_wiki_ln(self.df, 'last')
-        self.assertTrue(all(odf.sum(axis=1).round(1) == 1.0))
+        odf = pred_wiki_ln(self.df, "last")
+        self.assertTrue(
+            all(
+                odf[[col for col in odf.columns if col in race]].sum(axis=1).round(1)
+                == 1.0
+            )
+        )
         self.assertTrue(all(odf.true_race == odf.race))
 
     def test_pred_wiki_name(self):
-        odf = pred_wiki_name(self.df, 'last', 'first')
-        self.assertTrue(all(odf.sum(axis=1).round(1) == 1.0))
+        odf = pred_wiki_name(self.df, "last", "first")
+        self.assertTrue(
+            all(
+                odf[[col for col in odf.columns if col in race]].sum(axis=1).round(1)
+                == 1.0
+            )
+        )
         self.assertTrue(all(odf.true_race == odf.race))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- ethnicolr/tests/test_050_pred_nc.py
+++ ethnicolr/tests/test_050_pred_nc.py
@@ -14,22 +14,43 @@ from ethnicolr.pred_nc_reg_name import pred_nc_reg_name
 
 from . import capture
 
+race = [
+    "HL+A_mean",
+    "HL+B_mean",
+    "HL+I_mean",
+    "HL+M_mean",
+    "HL+O_mean",
+    "HL+W_mean",
+    "NL+A_mean",
+    "NL+B_mean",
+    "NL+I_mean",
+    "NL+M_mean",
+    "NL+O_mean",
+    "NL+W_mean",
+]
+
 
 class TestPredNC(unittest.TestCase):
-
     def setUp(self):
-        names = [{'last': 'smith', 'first': 'john', 'true_race': 'NL+M'},
-                 {'last': 'zhang', 'first': 'simon', 'true_race': 'NL+A'}]
+        names = [
+            {"last": "smith", "first": "john", "true_race": "NL+M"},
+            {"last": "zhang", "first": "simon", "true_race": "NL+A"},
+        ]
         self.df = pd.DataFrame(names)
 
     def tearDown(self):
         pass
 
     def test_pred_nc_reg_name(self):
-        odf = pred_nc_reg_name(self.df, 'last', 'first')
-        self.assertTrue(all(odf.sum(axis=1).round(1) == 1.0))
+        odf = pred_nc_reg_name(self.df, "last", "first")
+        self.assertTrue(
+            all(
+                odf[[col for col in odf.columns if col in race]].sum(axis=1).round(1)
+                == 1.0
+            )
+        )
         self.assertTrue(all(odf.true_race == odf.race))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
