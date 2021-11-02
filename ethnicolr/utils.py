7bcb83eecf1eba63c711c3fe04a318bb7798f59d
BasharNaji <balkuwai@gmail.com>
2021-11-02 13:08:08 -0700

    Set training=True and updated FL test script

--- ethnicolr/utils.py
+++ ethnicolr/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import VERBOSE
 import sys
 import pandas as pd
 import numpy as np
@@ -7,9 +8,10 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import sequence
 from pkg_resources import resource_filename
 
+
 def isstring(s):
     # if we use Python 3
-    if (sys.version_info[0] >= 3):
+    if sys.version_info[0] >= 3:
         return isinstance(s, str)
     # we use Python 2
     return isinstance(s, basestring)
@@ -27,8 +29,7 @@ def column_exists(df, col):
 
     """
     if col and (col not in df.columns):
-        print("The specify column `{0!s}` not found in the input file"
-              .format(col))
+        print("The specify column `{0!s}` not found in the input file".format(col))
         return False
     else:
         return True
@@ -47,7 +48,7 @@ def fixup_columns(cols):
     out_cols = []
     for col in cols:
         if type(col) == int:
-            out_cols.append('col{:d}'.format(col))
+            out_cols.append("col{:d}".format(col))
         else:
             out_cols.append(col)
     return out_cols
@@ -76,7 +77,7 @@ def find_ngrams(vocab, text, n):
 
     a = zip(*[text[i:] for i in range(n)])
     for i in a:
-        w = ''.join(i)
+        w = "".join(i)
         try:
             idx = vocab.index(w)
         except Exception as e:
@@ -84,19 +85,13 @@ def find_ngrams(vocab, text, n):
         wi.append(idx)
     return wi
 
-def transform_and_pred(df, 
-                       newnamecol, 
-                       cls, 
-                       VOCAB,
-                       RACE,
-                       MODEL,
-                       NGRAMS,
-                       maxlen, 
-                       num_iter, 
-                       conf_int):
+
+def transform_and_pred(
+    df, newnamecol, cls, VOCAB, RACE, MODEL, NGRAMS, maxlen, num_iter, conf_int
+):
 
     df[newnamecol] = df[newnamecol].str.strip().str.title()
-    df['rowindex'] = df.index
+    df["rowindex"] = df.index
 
     if cls.model is None:
         vdf = pd.read_csv(VOCAB)
@@ -106,38 +101,44 @@ def transform_and_pred(df,
         cls.race = rdf.race.tolist()
 
         cls.model = load_model(MODEL)
-    
+
     # build X from index of n-gram sequence
-    X = np.array(df[newnamecol].apply(lambda c:
-                                                 find_ngrams(cls.vocab,
-                                                             c, NGRAMS)))
+    X = np.array(df[newnamecol].apply(lambda c: find_ngrams(cls.vocab, c, NGRAMS)))
     X = sequence.pad_sequences(X, maxlen=maxlen)
 
     # define the quantile ranges for the confidence interval
     lower_perc = 0.5 - (conf_int / 2)
     upper_perc = 0.5 + (conf_int / 2)
-    
-    # Predict 
+
+    # Predict
     pdf = pd.DataFrame()
 
     for _ in range(num_iter):
-        pdf = pdf.append(pd.DataFrame(cls.model.predict(X, verbose=1)))
+        pdf = pdf.append(pd.DataFrame(cls.model(X, training=True)))
     print(cls.race)
     pdf.columns = cls.race
-    pdf['rowindex'] = pdf.index
+    pdf["rowindex"] = pdf.index
 
-    res = pdf.groupby('rowindex').agg([np.mean, 
-                                    np.std, 
-                                    lambda x: np.percentile(x, q =lower_perc), 
-                                    lambda x: np.percentile(x, q = upper_perc)]).reset_index()
-    res.columns = [f'{i}_{j}' for i, j in res.columns]
-    res.columns = res.columns.str.replace('<lambda_0>', 'lb')
-    res.columns = res.columns.str.replace('<lambda_1>', 'ub')
-    res.columns = res.columns.str.replace('rowindex_', 'rowindex')
+    res = (
+        pdf.groupby("rowindex")
+        .agg(
+            [
+                np.mean,
+                np.std,
+                lambda x: np.percentile(x, q=lower_perc),
+                lambda x: np.percentile(x, q=upper_perc),
+            ]
+        )
+        .reset_index()
+    )
+    res.columns = [f"{i}_{j}" for i, j in res.columns]
+    res.columns = res.columns.str.replace("<lambda_0>", "lb")
+    res.columns = res.columns.str.replace("<lambda_1>", "ub")
+    res.columns = res.columns.str.replace("rowindex_", "rowindex")
 
-    means = list(filter(lambda x:'_mean' in x, res.columns))
-    res['race'] = res[means].idxmax(axis = 1).str.replace("_mean", "")
-   
-    final_df = df.merge(res, on='rowindex', how='left')
+    means = list(filter(lambda x: "_mean" in x, res.columns))
+    res["race"] = res[means].idxmax(axis=1).str.replace("_mean", "")
+
+    final_df = df.merge(res, on="rowindex", how="left")
 
     return final_df
