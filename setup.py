27f88447de68cac8b64d0589297746a89e339dcb
Roman Imankulov <roman.imankulov@gmail.com>
2022-04-11 13:15:03 +0100

    Upgrade dependencies
    - Mark Python 3.9 and 3.10 as compatible
    - Add 3.10 to the test matrix
    - Unlock tensorflow and pandas
    
    Ref: #61

--- setup.py
+++ setup.py
@@ -92,6 +92,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities",
@@ -108,7 +110,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["tensorflow==2.5.3", "pandas>=1.3.0"],
+    install_requires=["tensorflow>=2.5.3", "pandas>=1.3.0"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
