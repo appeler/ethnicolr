07c5d25d6a36b6ad50fd6cae9e13c1f896adfa86
Roman Imankulov <roman.imankulov@gmail.com>
2022-06-02 10:14:23 +0100

    Relax Tensorflow requirements
    There's no Tensorflow 2.7.2 for Python 3.10. To make the project compatible
    with Python 3.10, we let the package manager install the latest version of
    Tensorflow 2.x
    
    Ref: https://pypi.org/project/tensorflow/2.7.2/#files

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["tensorflow==2.7.2", "pandas>=1.3.0"],
+    install_requires=["tensorflow>=2.7.2,<3", "pandas>=1.3.0"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
