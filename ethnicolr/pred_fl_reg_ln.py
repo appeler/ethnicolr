0e70e05d887136f3cb6763112fa3458f265c64c0
***** <gsood07@gmail.com>
2023-02-20 20:25:10 -0800

    Update pred_fl_reg_ln.py

--- ethnicolr/pred_fl_reg_ln.py
+++ ethnicolr/pred_fl_reg_ln.py
@@ -30,7 +30,7 @@ class FloridaRegLnModel:
     model = None
 
     @classmethod
-    def pred_fl_reg_ln(cls, df, namecol, num_iter=100, conf_int=1):
+    def pred_fl_reg_ln(cls, df: pd.DataFrame, namecol: str, num_iter: int=100, conf_int: float=1.0):
         """Predict the race/ethnicity by the last name using Florida voter
         model.
 
