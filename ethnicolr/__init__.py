1c0cdc81e714d97ab41a39472c8af1e70d046a57
***** <gsood07@gmail.com>
2023-03-20 04:34:50 -0700

    Update __init__.py

--- ethnicolr/__init__.py
+++ ethnicolr/__init__.py
@@ -4,7 +4,6 @@ from ethnicolr.pred_census_ln import pred_census_ln
 from ethnicolr.pred_wiki_ln import pred_wiki_ln
 from ethnicolr.pred_wiki_name import pred_wiki_name
 from ethnicolr.pred_fl_reg_ln import pred_fl_reg_ln
-from ethnicolr.pred_fl_reg_ln_cn_int import pred_fl_reg_ln_cn_int
 from ethnicolr.pred_fl_reg_name import pred_fl_reg_name
 from ethnicolr.pred_nc_reg_name import pred_nc_reg_name
 from ethnicolr.pred_fl_reg_ln_five_cat import pred_fl_reg_ln_five_cat
