5dc78c7834479b0b2f4b18a3f18524c6f893c1dd
***** <gsood07@gmail.com>
2023-04-13 13:09:59 -0700

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -7,29 +7,35 @@ from ethnicolr import census_ln
 st.title("Ethnicolr")
 
 # Add a file uploader widget for the user to upload a CSV file
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-lname_col = st.selectbox("Select column with last name", df.columns)
-year = st.selectbox("Select a year", [2000, 2010])
+def app():
+    # Set app title
+    st.title("My Streamlit App")
 
-# Add a button to trigger the transformation
-if st.button("Append Electoral Roll Data"):
-    # Check if a file was uploaded
-    if uploaded_file is not None:
-        # Read the uploaded CSV file into a Pandas DataFrame
+    # Upload CSV file
+    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+
+    # Load data
+    if file is not None:
         df = pd.read_csv(uploaded_file)
-        
-        # Use the package to transform the DataFrame
+        st.write("Data loaded successfully!")
+    else:
+        st.stop()
+
+    lname_col = st.selectbox("Select column with last name", df.columns)
+    year = st.selectbox("Select a year", [2000, 2010])
+
+    if st.button("Append Electoral Roll Data"):
+    # Use the package to transform the DataFrame
         transformed_df = census_ln(df, col=lname_col)
-        
-        # Display the transformed DataFrame as a table
         st.dataframe(transformed_df)
 
-        # Download results as CSV
         csv = transformed_df.to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()
         href = f'<a href="data:file/csv;base64,{b64}" download="results.csv">Download results</a>'
         st.markdown(href, unsafe_allow_html=True)
-    else:
-        # Display an error message if no file was uploaded
-        st.error("Please upload a CSV file to transform.")
+
+
+# Run the app
+if __name__ == "__main__":
+    app()
