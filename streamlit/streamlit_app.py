03ee6cfa5ae1106eecdcf1fe72fbddf0873f2a12
***** <gsood07@gmail.com>
2023-04-13 13:02:33 -0700

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -9,6 +9,9 @@ st.title("Ethnicolr")
 # Add a file uploader widget for the user to upload a CSV file
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
+lname_col = st.selectbox("Select column with last name", df.columns)
+year = st.selectbox("Select a year", [2000, 2010])
+
 # Add a button to trigger the transformation
 if st.button("Append Electoral Roll Data"):
     # Check if a file was uploaded
@@ -16,14 +19,11 @@ if st.button("Append Electoral Roll Data"):
         # Read the uploaded CSV file into a Pandas DataFrame
         df = pd.read_csv(uploaded_file)
         
-        lname_col = st.selectbox("Select column with last name", df.columns)
-        year = st.selectbox("Select a year", [2000, 2010])
-
         # Use the package to transform the DataFrame
         transformed_df = census_ln(df, col=lname_col)
         
         # Display the transformed DataFrame as a table
-        st.write(transformed_df)
+        st.dataframe(transformed_df)
 
         # Download results as CSV
         csv = transformed_df.to_csv(index=False)
